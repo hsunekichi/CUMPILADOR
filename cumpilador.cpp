9c976f0213f96f85f3dceddba0b4b2973341cf43
hsunekichi <102264744+hsunekichi@users.noreply.github.com>
2022-06-12 13:45:51 +0200

    Update cumpilador.cpp

--- cumpilador.cpp
+++ cumpilador.cpp
@@ -1,7 +1,7 @@
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
  *
  * Hecho por: Hugo Mateo
- * Última revisión: 26/04/2022
+ * Última revisión: 12/06/2022
  * 
  * Sintaxis de la configuración ASM: 
  * En la primera línea deberá poner BIN o HEX, dependiendo de la salida deseada.
